f75b3b2486f5267641646d535b9c95e43764fec7
AhmedElbossily <ahmed.fathy.elbossily@gmail.com>
2025-05-08 10:58:20 +0200

    update_slurm_cmakelists

--- run_SLURM.sh
+++ run_SLURM.sh
@@ -2,8 +2,8 @@
 
 ##Resource Request
 
-#SBATCH --job-name=fc_1_HR
-#SBATCH --partition=p2GPU40
+#SBATCH --job-name=FE_HR
+#SBATCH --partition=pGPU
 #SBATCH --ntasks=1
 #SBATCH --exclusive
 
@@ -13,8 +13,8 @@
 #SBATCH --time=72:00:00
 #SBATCH --mail-user=ahmed.elbossily@hereon.de
 #SBATCH --mail-type=END
-#SBATCH --output=/gpfs/work/elbossil/fc_1_HR/C-out.txt 
-#SBATCH --error=/gpfs/work/elbossil/fc_1_HR/C-error.txt
+#SBATCH --output=/gpfs/work/elbossil/FE_HR/C-out.txt 
+#SBATCH --error=/gpfs/work/elbossil/FE_HR/C-error.txt
 
-cd /gpfs/work/elbossil/fc_1_HR/build/
+cd /gpfs/work/elbossil/FE_HR/build/
 ./Mfree
